530faf2a6504c6903892acc247838da08511d400
jihaduttolibinn <jihaduttolibin02@gmail.com>
2026-03-02 14:06:54 +0700

    Mengganti public class yang awalnya DemoApplication menjadi Praktikum1Application

--- src/main/java/com/example/demo/Praktikum1Application.java
+++ src/main/java/com/example/demo/Praktikum1Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Praktikum1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Praktikum1Application.class, args);
 	}
 
 }
